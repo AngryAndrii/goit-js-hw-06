269cf2ab1f3ddb6c8daf2f2980b604a83b11395b
Andrii Chychkan <mywork1728@gmail.com>
2023-06-03 14:48:22 +0300

    started task 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,38 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector("#controls").firstElementChild;
+const createButton = input.nextElementSibling;
+const destroyButton = createButton.nextElementSibling;
+const boxes = document.querySelector('#boxes');
+
+let amount;
+
+function getAmountFunction() {
+  amount = input.value;
+  return amount;
+}
+
+function createBoxes(amount) {
+  console.log(`we create ${amount} divs`);
+  for (let i = 1; i <= amount; i += 1) {
+    let createdDivs = document.createElement('div');
+    createdDivs.classList.add('newDivs');
+    boxes.append(createdDivs);
+  }
+}
+
+
+function destroyBoxes() {
+  let allNewDivs = document.querySelectorAll(".newDivs");
+    for (let i = 0; i < a.length; i++) {
+      allNewDivs[i].remove();
+    }
+}
+
+input.addEventListener('input', getAmountFunction);
+createButton.addEventListener('click', () => {
+  createBoxes(amount);
+})
+destroyButton.addEventListener('click', destroyBoxes);
