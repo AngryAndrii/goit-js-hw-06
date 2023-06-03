cd5d632c69612c70b0510ae6bf7ffe4b384a054e
Andrii Chychkan <mywork1728@gmail>
2023-06-03 19:59:19 +0300

    finished all tasks

--- js/task-10.js
+++ js/task-10.js
@@ -1,13 +1,13 @@
+const input = document.querySelector("#controls").firstElementChild;
+const createButton = input.nextElementSibling;
+const destroyButton = createButton.nextElementSibling;
+const boxes = document.querySelector('#boxes');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
-
-const input = document.querySelector("#controls").firstElementChild;
-const createButton = input.nextElementSibling;
-const destroyButton = createButton.nextElementSibling;
-const boxes = document.querySelector('#boxes');
 
 let amount;
 
@@ -17,24 +17,30 @@ function getAmountFunction() {
 }
 
 function createBoxes(amount) {
-  console.log(`we create ${amount} divs`);
   for (let i = 1; i <= amount; i += 1) {
+    let d = 20 + (i * 10);
     let createdDivs = document.createElement('div');
     createdDivs.classList.add('newDivs');
+    createdDivs.style.width = `${d}px`;
+    createdDivs.style.height = `${d}px`;
+    createdDivs.style.backgroundColor = getRandomHexColor();
     boxes.append(createdDivs);
   }
 }
 
-
 function destroyBoxes() {
   let allNewDivs = document.querySelectorAll(".newDivs");
-    for (let i = 0; i < a.length; i++) {
+    for (let i = 0; i < allNewDivs.length; i++) {
       allNewDivs[i].remove();
-    }
+  }
+  amount = 0;
+  input.value = amount;
 }
 
 input.addEventListener('input', getAmountFunction);
+
 createButton.addEventListener('click', () => {
   createBoxes(amount);
 })
+
 destroyButton.addEventListener('click', destroyBoxes);
